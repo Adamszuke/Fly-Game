87d3945b8feb0bc07626ca2d5470a9794d17ce0f
Adamszuke <71903897+Adamszuke@users.noreply.github.com>
2024-07-17 11:59:41 -0300

    Fly Delay and Click counter

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 let button = document.getElementById('fly');
-let height = window.innerHeight - 50;
-let width = window.innerWidth - 50;
+let height = window.innerHeight - 100;
+let width = window.innerWidth - 100;
+let flyClicked = 0;
+let clickedText = document.getElementById('variable-text');
 
 button.addEventListener('mouseover', function(){
-    button.style.position = 'absolute';
-    button.style.top = Math.random() * height + 'px';
-    button.style.left = Math.random() * width + 'px';
+    setTimeout(function() {
+        button.style.position = 'absolute';
+        button.style.top = Math.random() * height + 'px';
+        button.style.left = Math.random() * width + 'px';
+    }, 1000)
 })
+
+button.addEventListener('click', function(){
+    flyClicked += 1;
+    clickedText.textContent = flyClicked;
+})
+
